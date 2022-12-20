f90a52bb10b9a323c75c379f3e913224f942c5e7
Natalya <akva888@mail.ru>
2022-12-20 12:26:25 +0300

    Work with kafka, simple example

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+
+var tokenSource = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    tokenSource.Cancel();
+};
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = args.Length > 0  ? args[0] : "test-consumer-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+var consumer = new ConsumerBuilder<Null, string>(consumerConfig)
+               .Build();
+
+const string topic = "test-topic";
+consumer.Subscribe(topic);
+
+while (!tokenSource.IsCancellationRequested)
+{
+    var message = consumer.Consume(new TimeSpan(0,0,1));
+    if (message != null)
+    {
+        Console.WriteLine(message.TopicPartitionOffset + " " + message.Value);
+    }
+}
+
+consumer.Close();
